7d37ceeacff855fe0eec07d650b3e8eabdf046d8
hyoguoo <hyoguoo@gmail.com>
2023-11-02 21:56:35 +0900

    init: init project

---
+++ src/main.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import {CheckoutPage} from "./pages/Checkout";
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import {SuccessPage} from "./pages/Success";
+import {FailPage} from "./pages/Fail";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <CheckoutPage/>,
+    },
+    {
+        path: "success",
+        element: <SuccessPage/>,
+    },
+    {
+        path: "fail",
+        element: <FailPage/>,
+    },
+]);
+
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+    <RouterProvider router={router}/>
+);
